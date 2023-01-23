c59703b09e8bf544a1bdee8c8380f47ca4fbd63c
Ivan <balashov.ivan.2015@mail.ru>
2023-01-23 07:21:24 +0300

    master

--- change_product.php
+++ change_product.php
@@ -19,35 +19,44 @@
     </header>
     <main>
         <?php
-        if($_SERVER['REQUEST_METHOD']==='POST'){
-            $errors = new ErrorsCollector();
-            $name = $_POST['name-input'];
-            $price = $_POST['price-input'];
-            $description = $_POST['description-input'];
-            $errMessage = "";
-            $db = new DBmanager();
-            $db->conect();
-            $query ="INSERT INTO products (price, NAME, description) VALUES ('$price', '$name', '$description');";
-            if(!($result = $db->query($query))){
-                $errors->addError("Не получилось добавить продукт в базу данных");
-            }
+            if($_SERVER['REQUEST_METHOD']==='POST'){
+                $errors = new ErrorsCollector();
+                $errMessage = "";
+                $id = $_SESSION['productId'];
+                $db = new DBmanager();
+                $db->conect();
+                $name = $_POST['name-input'];
+                $price = $_POST['price-input'];
+                $description = $_POST['description-input'];
+                $query ="UPDATE products SET NAME = '$name', price = $price, description = '$description'  WHERE products.id = $id;";
+                if(!($result = $db->query($query))){
+                    $errors->addError("Не получилось изменить продукт в базу данных");
+                }
 
-            $errMessage = implode("<br/>",$errors->getErrors());
-        }
+                $errMessage = implode("<br/>",$errors->getErrors());
+            }
         ?>
         <div id="log_section">
-            <form action="create_product.php" method="POST">
-                <label>
-                    <input class="str_input" type="text" name="name-input" placeholder="Название" required/>
-                </label><br/>
-                <label>
-                    <input class="str_input" type="number" name="price-input" placeholder="Цена" required/>
-                </label><br/>
-                <label>
-                    <textarea class="str_input" name="description-input" placeholder="Описание" required></textarea>
-                </label>
+            <?php
+                $id = $_SESSION['productId'];
+                $db = new DBmanager();
+                $db->conect();
+                $query ="SELECT * FROM products WHERE id = '$id';";
+                $result = $db->query($query);
+                $row = $result->fetch_array();
+            ?>
+            <form action="change_product.php" method="POST">
+                <label for="name-input">Название товара</label>
+                    <input class="str_input" type="text" name="name-input" value="<?= $row['NAME']?>" required/>
+                <br/>
+                <label for="price-input">Цена</label>
+                    <input class="str_input" type="number" name="price-input" value="<?= $row['price']?>" required/>
+                <br/>
+                <label for="description-input">Описание</label>
+                    <textarea class="str_input" name="description-input" required><?= $row['description']?> </textarea>
 
-                <input class="sbmt_input" type="submit" value="Создать">
+
+                <input class="sbmt_input" type="submit" value="изменить">
                 <?php
                 if(isset($errMessage)){
                     echo '<p style = "color: red; width: 100%; text-align: center;margin: 0;">'. $errMessage . '</p>';
